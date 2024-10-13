db5d7cbef65d009e5d397c4a9875c62726b91a5b
romazan26 <130604263+romazan26@users.noreply.github.com>
2024-10-13 21:20:57 +0300

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Роман on 13.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
